4a95b8618107814ddc1b98326b274c7ba77b68d1
Ryan Setiawan <ryansetiawan.rs70@gmail.com>
2026-03-25 23:33:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0b0f14",
+        foreground: "#f0f2f5",
+        primary: "#3fbec2",
+        secondary: "#1f2830",
+        muted: "#252e37",
+        border: "#242b32",
+      }
+    },
+  },
+  plugins: [],
+}
