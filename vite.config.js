5d0547a06a9f5fe61a2e5f18b29f8cf5d027285d
Ryan Setiawan <ryansetiawan.rs70@gmail.com>
2026-03-26 00:15:48 +0700

    fix blank netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: './', // 🔥 WAJIB
+  base: './', // 🔥 INI WAJIB
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
